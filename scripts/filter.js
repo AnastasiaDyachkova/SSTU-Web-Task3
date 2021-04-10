184826a767cd806afbf63ec59f7408cd73abbefb
AnastasiaDyachkova <dnastaa@gmail.com>
2021-04-10 22:26:23 +0400

    refactor filter

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,55 +1,56 @@
-function filter(){
+function filter() {
     document.querySelectorAll(".card-movie").forEach(e => e.remove());
-    
+
     let country = document.getElementById("iCountryFilter").value;
     let genres = document.getElementById("iGenreFilter").value;
     let year = document.getElementById("iYearFilter").value;
-    
+
     //Получаю коллекцию фильмов из LS
-    let allMoviesInLS =  JSON.parse(localStorage.getItem('movieCollecton'));
-    
+    let allMoviesInLS = JSON.parse(localStorage.getItem('movieCollection'));
+    console.log(allMoviesInLS);
     //Отрисовываю из LS карточки тех фильмов, которые соответствуют параметрам
-    if(allMoviesInLS!=null){
+    if (allMoviesInLS != null) {
+        console.log(country, genres, year);
 
-        if(country != "Любая" & genres != "Любой" & year != "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if(allMoviesInLS[i].country == country & allMoviesInLS[i].genre == genre & allMoviesInLS[i].yearF == year) addMovieOnPage(allMoviesInLS[i]);
-            }
-        }
-
-        if(country != "Любая" & genres != "Любой" & year == "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if(allMoviesInLS[i].country == country & allMoviesInLS[i].genre == genre) addMovieOnPage(allMoviesInLS[i]);
+        if (country != "Любая" & genres != "Любой" & year != "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].country == country & allMoviesInLS[i].genres == genres & allMoviesInLS[i].year == year) addMovieOnPage(allMoviesInLS[i]);
             }
         }
 
-        if(country != "Любая" & genre == "Любой" & year != "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if(allMoviesInLS[i].country == country & allMoviesInLS[i].yearF == year) addMovieOnPage(allMoviesInLS[i]);
+        if (country != "Любая" & genres != "Любой" & year == "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].country == country & allMoviesInLS[i].genres == genres) addMovieOnPage(allMoviesInLS[i]);
             }
         }
-        if(country != "Любая" & genre == "Любой" & year == "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if(allMoviesInLS[i].country == country) addMovieOnPage(allMoviesInLS[i]);
+
+        if (country != "Любая" & genres == "Любой" & year != "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].country == country & allMoviesInLS[i].year == year) addMovieOnPage(allMoviesInLS[i]);
             }
         }
-        if(country == "Любая" & genre != "Любой" & year != "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if(allMoviesInLS[i].genre == genre & allMoviesInLS[i].yearF == year) addMovieOnPage(allMoviesInLS[i]);
+        if (country != "Любая" & genres == "Любой" & year == "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].country == country) addMovieOnPage(allMoviesInLS[i]);
             }
         }
-        if(country == "Любая" & genre != "Любой" & year == "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if( allMoviesInLS[i].genre == genre) addMovieOnPage(allMoviesInLS[i]);
+        if (country == "Любая" & genres != "Любой" & year != "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].genres == genres & allMoviesInLS[i].year == year) addMovieOnPage(allMoviesInLS[i]);
             }
         }
-        if(country == "Любая" & genre == "Любой" & year != "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
-                if(allMoviesInLS[i].yearF == year) addMovieOnPage(allMoviesInLS[i]);
+        if (country == "Любая" & genres != "Любой" & year == "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].genres == genres) addMovieOnPage(allMoviesInLS[i]);
             }
         }
-        if(country == "Любая" & genre == "Любой" & year == "Любой") {
-            for(let i=0; i<allMoviesInLS.length; i++){
+        if (country == "Любая" & genres == "Любой" & year != "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
+                if (allMoviesInLS[i].year == year) addMovieOnPage(allMoviesInLS[i]);
+            }
+        }
+        if (country == "Любая" & genres == "Любой" & year == "Любой") {
+            for (let i = 0; i < allMoviesInLS.length; i++) {
                 addMovieOnPage(allMoviesInLS[i]);
             }
         }
